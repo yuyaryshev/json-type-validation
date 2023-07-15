225f89b56b99a1365adf49f765d8dc88fe4078cd
Yuri Yaryshev <yuyaryshev@gmail.com>
2023-07-15 09:51:17 +0300

    yya_new_rollup_config

---
+++ rollup.config.cjs
@@ -0,0 +1,37 @@
+// import dts from 'rollup-plugin-dts'
+// import esbuild from 'rollup-plugin-esbuild'
+const dts = require('rollup-plugin-dts').default
+const esbuild = require('rollup-plugin-esbuild').default
+
+const name = require('./package.json').main.replace(/\.js$/, '')
+
+const bundle = config => ({
+    ...config,
+    input: 'src/index.ts',
+    external: id => !/^[./]/.test(id),
+})
+
+module.exports.default = [
+    bundle({
+        plugins: [esbuild()],
+        output: [
+            {
+                file: `${name}.js`,
+                format: 'cjs',
+                sourcemap: true,
+            },
+            {
+                file: `${name}.mjs`,
+                format: 'es',
+                sourcemap: true,
+            },
+        ],
+    }),
+    bundle({
+        plugins: [dts()],
+        output: {
+            file: `${name}.d.ts`,
+            format: 'es',
+        },
+    }),
+]
